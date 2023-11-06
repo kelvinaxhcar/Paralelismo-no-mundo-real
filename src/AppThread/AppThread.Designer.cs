88b6fde0204d644d4caa1bf59856e95480ddbe6e
Kelvin Mateus Axhcar de Jesus <kaxhcar@gmail.com>
2023-11-06 10:45:59 -0300

    criado projeto pralelismo com tred

--- src/AppThread/AppThread.Designer.cs
+++ src/AppThread/AppThread.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            buttonExecutar = new Button();
+            richTextBox = new RichTextBox();
+            labelTempoFinal = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // buttonExecutar
+            // 
+            buttonExecutar.Location = new Point(606, 376);
+            buttonExecutar.Name = "buttonExecutar";
+            buttonExecutar.Size = new Size(94, 29);
+            buttonExecutar.TabIndex = 0;
+            buttonExecutar.Text = "Executar";
+            buttonExecutar.UseVisualStyleBackColor = true;
+            buttonExecutar.Click += AoClicarEmExecutar;
+            // 
+            // richTextBox
+            // 
+            richTextBox.Location = new Point(40, 38);
+            richTextBox.Name = "richTextBox";
+            richTextBox.Size = new Size(524, 297);
+            richTextBox.TabIndex = 1;
+            richTextBox.Text = "";
+            // 
+            // labelTempoFinal
+            // 
+            labelTempoFinal.AutoSize = true;
+            labelTempoFinal.Location = new Point(114, 385);
+            labelTempoFinal.Name = "labelTempoFinal";
+            labelTempoFinal.Size = new Size(0, 20);
+            labelTempoFinal.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 385);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Tempo";
+            label1.Click += label1_Click;
+            // 
+            // AppThread
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(714, 450);
+            Controls.Add(label1);
+            Controls.Add(labelTempoFinal);
+            Controls.Add(richTextBox);
+            Controls.Add(buttonExecutar);
+            Name = "AppThread";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonExecutar;
+        private RichTextBox richTextBox;
+        private Label labelTempoFinal;
+        private Label label1;
     }
 }
--- src/AppThread/AppThread.cs
+++ src/AppThread/AppThread.cs
@@ -1,10 +1,42 @@
+using App.Dominio.Servico;
+using App.Dominio;
+using System.Windows.Forms;
+
 namespace AppThread
 {
     public partial class AppThread : Form
     {
+        List<Tarefa> _tarefas;
+        ServicoDeTarefas _servicoDeTarefas;
         public AppThread()
         {
             InitializeComponent();
+            _servicoDeTarefas = new ServicoDeTarefas();
+            _tarefas = _servicoDeTarefas.ObterTarefas();
+        }
+
+        private void AoClicarEmExecutar(object sender, EventArgs e)
+        {
+            buttonExecutar.Enabled = false;
+            var execucoes = new List<string>();
+
+            var inicio = DateTime.Now;
+
+            _tarefas.ForEach(tarefa =>
+            {
+                var retorno = _servicoDeTarefas.Executar(tarefa);
+                execucoes.Add(retorno);
+            });
+            var fim = DateTime.Now;
+
+            richTextBox.Text = string.Join("\n", execucoes);
+            var segundos = fim - inicio;
+            labelTempoFinal.Text = segundos.TotalSeconds.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
